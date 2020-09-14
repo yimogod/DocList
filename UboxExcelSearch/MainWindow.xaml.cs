069d08d95d47f00ca19d1717c1ade375613e0793
liuzhibin <liuzhibin@kingsoft.com>
2020-09-14 19:48:15 +0800

    read excels

--- UboxExcelSearch/MainWindow.xaml.cs
+++ UboxExcelSearch/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Win32;
+using System.Collections.Generic;
 using System.IO;
 using System.Windows;
 
@@ -17,7 +18,30 @@ namespace UboxSearch
 		//查询按钮点击
 		private void btnSearch_Click(object sender, RoutedEventArgs e)
 		{
+			var txt = txtSearch.Text;
+			if (string.IsNullOrWhiteSpace(txt))
+			{
+				MessageBox.Show("输入正确的查询内容!");
+				return;
+			}
 
+			//保存查询值
+			var key = ReadRegistryKey();
+			if (key == null) return;
+			key.SetValue("SearchContent", txt);
+
+
+			var path = GetValidPath(txtPath.Text);
+			if (path == null)
+			{
+				MessageBox.Show("请先设置 Excel 所在目录!");
+				return;
+			}
+
+			excelList.Clear();
+			DirectoryInfo root = new DirectoryInfo(path);
+			GetExcelFiles(root);
+			var n = excelList.Count;
 		}
 
 		private void ListView_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
@@ -27,17 +51,8 @@ namespace UboxSearch
 
 		private void btnSet_Click(object sender, RoutedEventArgs e)
 		{
-			var path = txtPath.Text;
-			bool validFolder = Directory.Exists(path);
-
-			bool validFile = File.Exists(path);
-			if (validFile)
-			{
-				path = Path.GetDirectoryName(path);
-				validFolder = Directory.Exists(path);
-			}
-
-			if (validFolder)
+			var path = GetValidPath(txtPath.Text);
+			if (path != null)
 			{
 				var key = ReadRegistryKey();
 				if (key == null) return;
@@ -55,9 +70,16 @@ namespace UboxSearch
 			if (key == null) return;
 
 			var value = key.GetValue("ExcelPath") as string;
-			if (string.IsNullOrEmpty(value)) return;
+			if (!string.IsNullOrWhiteSpace(value))
+			{
+				txtPath.Text = value;
+			}
 
-			txtPath.Text = value;
+			value = key.GetValue("SearchContent") as string;
+			if (!string.IsNullOrWhiteSpace(value))
+			{
+				txtSearch.Text = value;
+			}
 		}
 
 		private RegistryKey ReadRegistryKey()
@@ -71,6 +93,37 @@ namespace UboxSearch
 			}
 
 			return keyRoot;
+		}
+
+		private string GetValidPath(string path)
+		{
+			bool validFolder = Directory.Exists(path);
+
+			bool validFile = File.Exists(path);
+			if (validFile)
+			{
+				path = Path.GetDirectoryName(path);
+				validFolder = Directory.Exists(path);
+			}
+
+			if (validFolder) return path;
+			return null;
+		}
+
+		private List<FileInfo> excelList = new List<FileInfo>();
+		private void GetExcelFiles(DirectoryInfo root)
+		{
+			var folders = root.GetDirectories();
+			foreach (var folder in folders)
+			{
+				GetExcelFiles(folder);
+			}
+
+			var files = root.GetFiles();
+			foreach (FileInfo f in files)
+			{
+				if (f.Extension.Equals(".xlsx")) excelList.Add(f);
+			}
 		}
 	}
 }
