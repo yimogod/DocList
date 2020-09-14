48a2dd120b1f70590ec183a73d0a50cccc4420a8
liuzhibin <liuzhibin@kingsoft.com>
2020-09-14 18:57:27 +0800

    init ubox search tool

---
+++ UboxExcelSearch/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+
+namespace UboxSearch
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		//查询按钮点击
+		private void btnSearch_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void ListView_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+		{
+
+		}
+
+		private void btnSet_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+	}
+}
